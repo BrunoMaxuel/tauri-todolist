cd3d889e9c333e908ec9713c28e24af34a68c6ed
Bruno maxuel <brunomaxuel2@gmail.com>
2025-05-25 16:10:39 -0300

    Mudança na estrutura

--- src/main.tsx
+++ src/main.tsx
@@ -2,11 +2,10 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import DefaultLayout from "./Layouts/DefaultLayout";
 import "./assets/css/TailwindCSS.css";
+import { App } from "./App";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <DefaultLayout>
-      <div>Test</div>
-    </DefaultLayout>
+    <App/>
   </React.StrictMode>,
 );
