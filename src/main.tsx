1ac4d87ec46e6894a5eff37ea5f9b34091b82df0
Bruno maxuel <brunomaxuel2@gmail.com>
2025-05-25 15:51:48 -0300

    Estrutura basica criada

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./Layouts/DefaultLayout";
+import DefaultLayout from "./Layouts/DefaultLayout";
 import "./assets/css/TailwindCSS.css";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <App />
+    <DefaultLayout>
+      <div>Test</div>
+    </DefaultLayout>
   </React.StrictMode>,
 );
